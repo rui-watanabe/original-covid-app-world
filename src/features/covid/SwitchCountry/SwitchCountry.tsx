ccce8fc3e5bff53d14b93db6afab95145fd9a2e3
rui-watanabe <wataru610806@gmail.com>
2021-08-08 20:14:41 +0900

    add container #12

--- src/features/covid/SwitchCountry/SwitchCountry.tsx
+++ src/features/covid/SwitchCountry/SwitchCountry.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { NativeSelect, FormControl } from "@material-ui/core";
 import { useDispatch } from "react-redux";
-import { fetchAsyncGet, fetchAsyncGetCountry } from "../covidSlice";
+import { fetchAsyncGetCountry } from "../covidSlice";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
